b057a885cb610743daeec6de33321a1d7021069d
omryn <omry@pop-os.localdomain>
2019-09-09 12:33:28 +1000

    added stores and adjusted old samples

--- packages/example-playground/src/examples/reactive/source.tsx
+++ packages/example-playground/src/examples/reactive/source.tsx
@@ -1,4 +1,4 @@
-import { TSXAir, bind, lifecycle } from '../../framework';
+import { TSXAir, bind } from '../../framework';
 
 export const Derived = TSXAir((props:{a:string}) => {
     // The view is updated iff props.a is changed
@@ -8,7 +8,11 @@ export const Derived = TSXAir((props:{a:string}) => {
 export const DerivedWithState = TSXAir((props: { a: string }) => {
     let count = store(0);
     const first = store(props.a);
+
+    console.log(count); // 0
     bind.when(props.a, () => count++);
-    
+    console.log(count); // 1
+
     return <div>{'a = ' + props.a}<div>A was changed {count} times</div></div>;
 });
+
