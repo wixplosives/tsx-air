92ecf4b221bc09b8b568d9a6b1c89134c3537a5a
omryn <omry@pop-os.localdomain>
2019-10-30 12:35:26 +0200

    fixed test + test config

--- packages/compiler/src/visitors/jsx.test.ts
+++ packages/compiler/src/visitors/jsx.test.ts
@@ -18,7 +18,7 @@ describe('jsx visitors', () => {
             const texts = getTextBlockChildren(jsx, { stop: () => void (0), ignoreChildren: () => void (0), report: () => void (0) });
             expect(texts).to.have.length(2);
             const dynamicText = texts![0].map((i: Node) => i.getText().trim()).join('^^^');
-            const staticText = texts![0].map((i: Node) => i.getText().trim()).join('^^^');
+            const staticText = texts![1].map((i: Node) => i.getText().trim()).join('^^^');
             expect(dynamicText).to.equal('this is a text:^^^{props.text}^^^!!!');
             expect(staticText).to.equal('this is a static text');
         });
