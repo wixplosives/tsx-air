d89a598beed5ba709532a0061e2a5dc9c339907a
omryn <omry@pop-os.localdomain>
2020-01-02 22:44:37 +0100

    fixed imports

--- packages/examples/src/examples/04.zoom/runner.ts
+++ packages/examples/src/examples/04.zoom/runner.ts
@@ -1,3 +1,5 @@
+import { render } from '@tsx-air/framework';
+import { Zoom } from './index.source';
 
 export const runExample = (target: HTMLElement) => {
     render(target, Zoom, { url: 'https://i.pinimg.com/originals/ba/ea/e4/baeae441e72112a3154f840b70b930ea.jpg' });
