12130f87c86dccad57b880252aee8b98d423117c
omryn <omry@pop-os.localdomain>
2020-01-23 17:04:44 +0100

    should fail in CI

--- .mocharc.js
+++ .mocharc.js
@@ -2,6 +2,6 @@ module.exports = {
     require: ['@ts-tools/node/r', 'tsconfig-paths/register'],
     extension: ['js', 'json', 'ts', 'tsx'],
     colors: true,
-    retries: process.env.CI ? 10 : 1,
-    timeout: process.env.CI ? 10000 : 2000
+    retries: process.env.CI ? 1 : 1,
+    timeout: process.env.CI ? 10 : 2000
 };
