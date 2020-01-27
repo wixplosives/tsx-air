98b8ed59e3e33afe3726f8b89d0d3b06fa830c03
omryn <omry@pop-os.localdomain>
2020-01-27 21:46:06 +0100

    added CI timing multiplier

--- packages/utils/src/promised.test.ts
+++ packages/utils/src/promised.test.ts
@@ -2,11 +2,12 @@ import { duration, delay } from './promises';
 import { expect } from 'chai';
 describe('promises', () => {
     describe('delay', () => {
+        const m = process.env.CI ? 3 : 1;
         it('should resolve after the set timeout', async () => {
-            expect(await duration(delay(21))).to.be.within(20, 50);
+            expect(await duration(delay(21 * m))).to.be.within(20 * m, 50 * m);
         });
         it('should throw when canceled', async () => {
-            const dl = delay(100);
+            const dl = delay(100 * m);
             try {
                 dl.cancel();
                 await dl;
@@ -15,16 +16,16 @@ describe('promises', () => {
             }
         });
         it('should fire when finish is called', async () => {
-            const dl = delay(100);
+            const dl = delay(100 * m);
             const actualDuration = duration(dl);
             dl.finish();
-            expect(await actualDuration).to.be.lessThan(100);
+            expect(await actualDuration).to.be.lessThan(100 * m);
         });
         it('should reset the timer with setTimeout', async () => {
-            const dl = delay(100);
+            const dl = delay(100 * m);
             const actualDuration = duration(dl);
-            dl.setTimeout(11);
-            expect(await actualDuration).to.be.within(10, 50);
+            dl.setTimeout(11 * m);
+            expect(await actualDuration).to.be.within(10 * m, 50 * m);
         });
     });
 });
