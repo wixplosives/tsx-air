5d7f0ac609e8bb3953ba142b7ad292bb63aabb33
omryn <omry.nachman@gmail.com>
2020-04-16 13:16:06 +0300

    added test.helpers

--- packages/compiler-utils/src/analyzers/test.helpers.ts
+++ packages/compiler-utils/src/analyzers/test.helpers.ts
@@ -5,7 +5,7 @@ import { find } from '../ast-utils/scanner';
 import { CompDefinition } from './types';
 
 export const getCompDef = (code: string) => {
-    const ast = asAst(code);
+    const ast = asAst(code, true);
     const tsxairNode = find(ast, node => ts.isCallExpression(node));
     const comp = compDefinition(tsxairNode).tsxAir as CompDefinition;
 
