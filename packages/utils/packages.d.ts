b2793edc06db78784aac66f85947cd187aa207fd
omry nachman <omry.nachman@gmail.com>
2020-04-22 16:11:59 +0300

    added winssafepath

--- packages/utils/packages.d.ts
+++ packages/utils/packages.d.ts
@@ -1,1 +1,2 @@
 export declare function packagePath(name: string, ...innerPath: string[]): string;
+export declare function winSafePath(path: string):string;
--- packages/utils/packages.js
+++ packages/utils/packages.js
@@ -1,4 +1,8 @@
 const { join, dirname } = require('path');
 
-exports.packagePath = (name, ...innerPath) =>
-    join(dirname(require.resolve(join(name, 'package.json'))), ...innerPath)
+const winSafePath = (path) => path.substr(0,1).toUpperCase() + path.substr(1);
+exports.packagePath = (name, ...innerPath) => 
+    winSafePath(join(dirname(require.resolve(join(name, 'package.json'))), ...innerPath));
+
+exports.winSafePath = winSafePath;
+
