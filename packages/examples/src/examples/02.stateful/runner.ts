125222ca46246125f2d8cfaacaa5d518087d6ef1
omryn <omry.nachman@gmail.com>
2020-06-25 16:05:16 +0300

    examples fix

--- packages/examples/src/examples/02.stateful/runner.ts
+++ packages/examples/src/examples/02.stateful/runner.ts
@@ -2,7 +2,7 @@ import { StatefulComp } from './index.source';
 import { render } from '@tsx-air/framework';
 
 export const runExample = (target: HTMLElement) => {
-    render(target, StatefulComp, { initialState: 'Click me!' });
+    render(StatefulComp, { initialState: 'Click me!' }, undefined, target, 'append')
 };
 
 export { StatefulComp as Component };
