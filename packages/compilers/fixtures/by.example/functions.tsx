25e0350ff08dd97eedf0c0e4250cf71fe8b8e340
omryn <omry.nachman@gmail.com>
2020-07-01 16:36:00 +0300

    removed dead fixtures

--- packages/compilers/fixtures/by.example/functions.tsx
+++
@@ -1,8 +0,0 @@
-import { store, TSXAir } from '@tsx-air/framework';
-
-export const NamedFunction = TSXAir(() => {
-    const store1 = store({ a: 'initial' });
-    const vol = `volatile`;
-    const getDisplayedStr = (b:string) => `${store1.a} ${vol} ${b}`;
-    return <div>{getDisplayedStr('param')}</div>;
-});
--- packages/compilers/fixtures/by.example/stateless.nested.tsx
+++
@@ -1,7 +0,0 @@
-import { TSXAir } from '@tsx-air/framework';
-
-export const Child = TSXAir((props: { b: string }) =>
-    <div>{props.b}</div>);
-
-export const Parent = TSXAir((props: { a: string }) =>
-    <div><Child b={props.a} /></div>);
