03c913fe4269d7c44ac590968c5cd4f9e20fe337
Nadav Abrahami <nadav@wix.com>
2020-07-02 14:14:08 +0300

    added more examples with children

--- packages/examples/src/examples/TODO.05.dynamic.gallery/index.source.tsx
+++ packages/examples/src/examples/TODO.05.dynamic.gallery/index.source.tsx
@@ -9,18 +9,70 @@ export const Gallery = TSXAir((props: { baseUrl: string }) => {
             { dataId: 3, src: 'weird.jpg' }
         ]
     });
-    const deleteButton = ((imgName: string) => {
+    const deleteButton = (imgName: string) => {
         const t = imgName;
-        return <button onClick={
-            () => state.images = state.images.filter(i => i.src !== t)
-        } disabled={state.images.find(i => i.src === imgName)} />;
-    });
+        return (
+            <button
+                onClick={() => (state.images = state.images.filter(i => i.src !== t))}
+                disabled={state.images.find(i => i.src === imgName)}
+            />
+        );
+    };
 
-    return <div className="gallery">
-        {state.images.map((img, i) => <div key={img.dataId + ''}>
-            <img src={`/${props.baseUrl}/${img}`}
-                alt={`image ${i + 1}/${state.images.length}`} />
-            {deleteButton(img.src)}
-        </div>)}
-    </div>;
+    return (
+        <div className="gallery">
+            {state.images.map((img, i) => (
+                <div key={img.dataId + ''}>
+                    <img src={`/${props.baseUrl}/${img}`} alt={`image ${i + 1}/${state.images.length}`} />
+                    {deleteButton(img.src)}
+                </div>
+            ))}
+        </div>
+    );
+});
+
+import { TSXAir, store } from '@tsx-air/framework';
+
+export const Delimiter = TSXAir((props: { children: TSXAir.Child[] }) => {
+    return (
+        <div className="root">
+            {props.children.map((child, i) => (
+                <div key={TSXAir.key(child)}>
+                    <hr />
+                    {child}
+                </div>
+            ))}
+        </div>
+    );
+});
+
+export const DelimiterParent = TSXAir((props: { baseUrl: string }) => {
+    const state = store({
+        images: [
+            { dataId: 0, src: 'bunny.jpg' },
+            { dataId: 1, src: 'gradient.jpg' },
+            { dataId: 2, src: 'pretty-boy.jpg' },
+            { dataId: 3, src: 'weird.jpg' }
+        ]
+    });
+    const deleteButton = (imgName: string) => {
+        const t = imgName;
+        return (
+            <button
+                onClick={() => (state.images = state.images.filter(i => i.src !== t))}
+                disabled={state.images.find(i => i.src === imgName)}
+            />
+        );
+    };
+
+    return (
+        <Delimiter>
+            {state.images.map((img, i) => (
+                <div key={img.dataId + ''}>
+                    <img src={`/${props.baseUrl}/${img}`} alt={`image ${i + 1}/${state.images.length}`} />
+                </div>
+            ))}
+            <div>click to subscribe</div>
+        </Delimiter>
+    );
 });
