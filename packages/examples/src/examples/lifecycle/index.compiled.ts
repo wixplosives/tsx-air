a3a40da9a9d204bfc39eb0105242440d167b8a8d
omryn <omry.nachman@gmail.com>
2020-08-31 19:15:25 +0300

    created a manually compiled version of lifecycle example

--- packages/examples/src/examples/lifecycle/index.compiled.ts
+++ packages/examples/src/examples/lifecycle/index.compiled.ts
@@ -2,42 +2,48 @@ import { CompCreator } from '@tsx-air/framework/src/api/types';
 import { RenderTarget, ComponentApi } from '@tsx-air/framework/src';
 
 interface Props {
-    name: string;
+    title: string;
 }
-export const ChildComp: CompCreator<Props> = (props: Props) => ({
+export const Clock: CompCreator<Props> = (props: Props) => ({
     props
 });
-ChildComp.render = (props: Props, target?: HTMLElement, add?: RenderTarget) => {
+Clock.render = (props: Props, target?: HTMLElement, add?: RenderTarget) => {
     if (!target || add !== 'append') {
         throw new Error('Now supported in this example');
     }
-    const child = document.createElement('div');
-    child.textContent = `Child: ${props.name}`;
-    child.classList.add('child');
-    target.append(child);
-    return {
-        updateProps: (p: Props) => {
-            child.textContent = `Child: ${p.name}`;
-        },
-    } as ComponentApi<Props>;
-};
+    const state = {
+        time: 'Not set',
+        area: 0,
+        updatesCount: 0
+    };
+    const clock = document.createElement('div');
+    clock.innerHTML = ` <a href="#">
+    <h1>${props.title}</h1></a>
+    <h2>Title area: ${state.area}px²</h2>
+    <h3>Title updated ${state.updatesCount} times</h3>
+    <div class="time">${state.time}</div>`;
 
-export const ParentComp: CompCreator<Props> = (props: Props) => ({
-    props
-});
-ParentComp.render = (props: Props, target?: HTMLElement, add?: RenderTarget) => {
-    if (!target || add !== 'append') {
-        throw new Error('Now supported in this example');
-    }
-    const parent = document.createElement('div');
-    parent.textContent = `Parent: ${props.name}`;
-    parent.classList.add('parent');
-    const child = ChildComp.render(props, parent as HTMLElement, 'append');
-    target.append(parent);
+    const update = () => {
+        const { width, height } = clock.querySelector('h1')!.getClientRects()[0];
+        state.updatesCount++;
+        state.area = Math.round(width * height);
+        clock.querySelector('h2')!.textContent = `Title area: ${state.area}px²`;
+        clock.querySelector('h3')!.textContent = `Title updated ${state.updatesCount} times`;
+    };
+    target.append(clock);
+
+    requestAnimationFrame(update);
+
+    window.setInterval(() => {
+        state.time = new Date().toTimeString();
+        clock.querySelector('.time')!.textContent = state.time;
+    }, 100);
+    clock.querySelector('a')?.focus();
+
     return {
         updateProps: (p: Props) => {
-            parent.childNodes[0].textContent = `Parent: ${p.name}`;
-            child.updateProps(p);
+            clock.querySelector('h1')!.textContent = props.title = p.title;
+            requestAnimationFrame(update);
         },
     } as ComponentApi<Props>;
 };
