99666989ada03213ce87545642bbeb3392ae03e9
omryn <omry.nachman@gmail.com>
2020-09-23 15:39:04 +0300

    added test suite for hooks

--- packages/examples/src/examples/hooks/suite.ts
+++ packages/examples/src/examples/hooks/suite.ts
@@ -1,16 +1,44 @@
 import { ExampleSuiteApi, Features, feature } from '@tsx-air/types';
-import { htmlMatch } from '@tsx-air/testing';
+import { htmlMatch, waitAnimationFrame } from '@tsx-air/testing';
 import { expect } from 'chai';
+import { Page } from 'puppeteer';
 
 export const features: Features = [
     feature('hook')
 ];
 
 export function suite(api: ExampleSuiteApi) {
+    const getEyeBallsTranslation = async (page: Page) => {
+        await waitAnimationFrame(page);
+        const styles = await page.$$eval('.eye > div', eyeBalls => eyeBalls.map(e => e.getAttribute('style')));
+        const eyes = styles.map(s => s!.match(/-?\d+\.?\d+/g)?.map(i => parseInt(i))) as number[][];
+        if (!eyes || eyes.length === 0 || eyes[0].length === 0) {
+            return {
+                left: { x: 0, y: 0 },
+                right: { x: 0, y: 0 }
+            };
+        }
+        return {
+            left: { x: eyes[0][0], y: eyes[0][1] },
+            right: { x: eyes[1][0], y: eyes[1][1] }
+        };
+    };
+
     it('follows the mouse cursor', async () => {
         const page = await api.afterLoading;
-        await page.mouse.move(0,0);
-        
-        
+        await htmlMatch(page, { cssQuery: '.eye', pageInstances: 2 });
+        const face = JSON.parse(await page.$eval('.face', el => JSON.stringify((el as any).getBoundingClientRect())));
+        await page.mouse.move(face.left, face.top);
+        let eyes = await getEyeBallsTranslation(page);
+        expect(eyes.right.x).to.be.approximately(-21, 2);
+        expect(eyes.right.y).to.be.approximately(-7, 2);
+        expect(eyes.left.x).to.be.approximately(-10, 2);
+        expect(eyes.left.y).to.be.approximately(-11, 2);
+        await page.mouse.move(face.right, face.bottom);
+        eyes = await getEyeBallsTranslation(page);
+        expect(eyes.right.x).to.be.approximately(5, 2);
+        expect(eyes.right.y).to.be.approximately(21, 2);
+        expect(eyes.left.x).to.be.approximately(10, 2);
+        expect(eyes.left.y).to.be.approximately(12, 2);
     });
 }
